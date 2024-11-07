961cd8c354877442d6feb5797e83b75b0ff09c0a
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-07 21:58:24 +0700

    fix: implements ssg

---
+++ 01-pages-router/src/pages/product/static.tsx
@@ -0,0 +1,27 @@
+import ProductView from "@/views/Product";
+import { ProductType } from "@/types/product.type";
+
+const ProductPage = () => {
+  // const ProductPage = (props: { products: ProductType[] }) => {
+  //   const { products } = props;
+
+  return (
+    <div>
+      <ProductView products={[]} />
+    </div>
+  );
+};
+
+export default ProductPage;
+
+// export async function getStaticProps() {
+//   // fetch data
+//   const res = await fetch("http://localhost:3000/api/product");
+//   const response = await res.json();
+
+//   return {
+//     props: {
+//       products: response.data,
+//     },
+//   };
+// }
