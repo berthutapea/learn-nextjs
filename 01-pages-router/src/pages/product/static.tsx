5c4d0f5d43cb0574bebf93aceeb59b4a7c50bed1
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-09 01:05:23 +0700

    incremental static regeneration

--- 01-pages-router/src/pages/product/static.tsx
+++ 01-pages-router/src/pages/product/static.tsx
@@ -1,27 +1,26 @@
+import { ProductType } from "@/types/product.type";
 import ProductView from "@/views/Product";
-// import { ProductType } from "@/types/product.type";
 
-const ProductPage = () => {
-  // const ProductPage = (props: { products: ProductType[] }) => {
-  //   const { products } = props;
+const ProductPage = (props: { products: ProductType[] }) => {
+  const { products } = props;
 
   return (
     <div>
-      <ProductView products={[]} />
+      <ProductView products={products} />
     </div>
   );
 };
 
 export default ProductPage;
 
-// export async function getStaticProps() {
-//   // fetch data
-//   const res = await fetch("http://localhost:3000/api/product");
-//   const response = await res.json();
+export async function getStaticProps() {
+  // fetch data
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
 
-//   return {
-//     props: {
-//       products: response.data,
-//     },
-//   };
-// }
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
