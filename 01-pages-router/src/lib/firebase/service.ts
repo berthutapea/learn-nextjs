97fcdf4e4f85f2830a0dc4fcbbfd75b9ccfef79d
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-08 22:46:58 +0700

    rendering dynamic routes

--- 01-pages-router/src/lib/firebase/service.ts
+++ 01-pages-router/src/lib/firebase/service.ts
@@ -1,4 +1,10 @@
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
 import app from "./init";
 
 const firestore = getFirestore(app);
@@ -10,6 +16,13 @@ export async function retrieveData(CollectionName: string) {
     id: doc.id,
     ...doc.data(),
   }));
+
+  return data;
+}
+
+export async function retrieveDataById(CollectionName: string, id: string) {
+  const snapshot = await getDoc(doc(firestore, CollectionName, id));
+  const data = snapshot.data();
 
   return data;
 }
