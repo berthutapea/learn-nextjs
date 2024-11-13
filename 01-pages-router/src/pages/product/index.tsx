2a4de03c2ad196a32c6acb75871726100df3bc23
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-13 16:05:51 +0700

    unit testing

--- 01-pages-router/src/pages/product/index.tsx
+++ 01-pages-router/src/pages/product/index.tsx
@@ -19,7 +19,7 @@ const ProductPage = () => {
 
   return (
     <div>
-      <ProductView products={isLoading ? [] : data.data} />
+      <ProductView products={isLoading ? [] : data?.data} />
     </div>
   );
 };
