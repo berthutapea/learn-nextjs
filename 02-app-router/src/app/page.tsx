00c7774f278525e7665648a7a08e6cb257789416
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-28 17:31:55 +0700

    seo & metadata

--- 02-app-router/src/app/page.tsx
+++ 02-app-router/src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Gilbert Hutapea",
+  description: "Aplikasi untuk belajar Next JS",
+  authors: [{ name: "Gilbert Hutapea", url: "http://localhost:3000" }],
+  icons: {
+    icon: "/icon.png",
+  },
+  openGraph: {
+    title: "Home - Gilbert Hutapea",
+  },
+};
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
